9be58a67af46e93b28d51e5be888e2d5027777f9
Steven <gonghaima@hotmail.com>
2022-01-09 19:13:29 +1100

    with Yup

--- index.ts
+++ index.ts
@@ -1,27 +1,42 @@
-const fs = require('fs');
-interface Result {
-  results: {
-    id: number;
-    name: string;
-    job: string;
-  }[];
-}
+import * as yup from "yup";
+import type { TypeOf } from "yup";
+import fs from "fs";
+
+const resultSchema = yup
+  .object()
+  .required()
+  .shape({
+    results: yup
+      .array()
+      .required()
+      .of(
+        yup.object().required().shape({
+          name: yup.string().required(),
+          id: yup.number().required(),
+          job: yup.string().required(),
+        })
+      ),
+  });
+
+type Result = TypeOf<typeof resultSchema>;
 
 const printJobs = (results: Result) => {
-  results?.results?.forEach(({ job }) => {
-    console.log(job);
-  });
+  if (resultSchema.validateSync(results)) {
+    console.log(results.results?.map(({ job }) => `${job}`).join("\n"));
+  } else {
+    console.error("Bad input");
+  }
 };
 
 // printJobs({
 //   results: [
 //     {
 //       id: 1,
-//       name: 'John',
-//       job: 'developer',
+//       name: "Jack",
+//       job: "Programmer",
 //     },
 //   ],
 // });
 
-const data: Result = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const data: Result = JSON.parse(fs.readFileSync("./data.json").toString());
 printJobs(data);
